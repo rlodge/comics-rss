35166f5aa986aa699ad68567b7e18f47c5dee56f
Ross Lodge <ross.lodge@electronicmuse.com>
2025-06-26 08:13:06 -0700

    Change lambda input.  Wrap M&E in try/catch.

--- src/main/kotlin/com/electronicmuse/comicsrss/Lambda.kt
+++ src/main/kotlin/com/electronicmuse/comicsrss/Lambda.kt
@@ -8,11 +8,11 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import kotlinx.coroutines.runBlocking
 import java.nio.charset.StandardCharsets
 
-class Lambda : RequestHandler<Map<String, String>, String> {
+class Lambda : RequestHandler<Map<String, Any>, String> {
 
     var fileAccess: FileAccess? = null
 
-    override fun handleRequest(input: Map<String, String>?, context: Context?): String? {
+    override fun handleRequest(input: Map<String, Any>?, context: Context?): String? {
         val mapper: ObjectMapper = YAMLMapper()
         mapper.findAndRegisterModules()
         val configuration: Configuration = mapper.readValue(javaClass.classLoader.getResource("config.yaml")!!)
--- src/main/kotlin/com/electronicmuse/comicsrss/providers/MadamAndEveComicLinkProvider.kt
+++ src/main/kotlin/com/electronicmuse/comicsrss/providers/MadamAndEveComicLinkProvider.kt
@@ -2,8 +2,10 @@ package com.electronicmuse.comicsrss.providers
 
 import com.electronicmuse.comicsrss.ComicLink
 import com.electronicmuse.comicsrss.ComicLinkProvider
+import com.fasterxml.jackson.annotation.JsonIgnore
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.slf4j.LoggerFactory
 import java.io.File
 import java.time.LocalDate
 import java.time.OffsetDateTime
@@ -16,32 +18,41 @@ data class MadamAndEveComicLinkProvider(
     val archivePath: String,
 ) : ComicLinkProvider {
 
+    @JsonIgnore
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @JsonIgnore
     val regex = ".*(\\d{4}-\\d{2}-\\d{2})-(.+)".toRegex()
 
     override fun provideLinks(nowSupplier: Supplier<OffsetDateTime>): List<ComicLink> {
-        val doc: Document = if (archivePath.lowercase().startsWith("http")) {
-            Jsoup.connect(archivePath).get()
-        } else {
-            Jsoup.parse(File(archivePath))
-        }
-
-        val anchors = doc.select("div[class='media-item Madam & Eve'] > a")
-        return anchors
-            .flatMap { element ->
-                val link: String? = element.attr("href")
-                if (!link.isNullOrBlank()) {
-                    val matchResult = regex.find(link)
-                    val (comicDate: OffsetDateTime, comicTitle: String) = if (matchResult != null) {
-                        val ld = DateTimeFormatter.ISO_LOCAL_DATE.parse(matchResult.groupValues[1])
-                        LocalDate.from(ld).atTime(0, 0).atZone(ZoneId.of("Africa/Johannesburg")).toOffsetDateTime() to matchResult.groupValues[2].formatTitle()
-                    } else {
-                        nowSupplier.get() to link.trimEnd('/').replace(".+/(.+)".toRegex(), "$1").formatTitle()
-                    }
-                    listOf(ComicLink(comicDate, "M&E - $comicTitle", link))
-                } else {
-                    emptyList()
-                }
+        return try {
+            val doc: Document = if (archivePath.lowercase().startsWith("http")) {
+                Jsoup.connect(archivePath).get()
+            } else {
+                Jsoup.parse(File(archivePath))
             }
+
+            val anchors = doc.select("div[class='media-item Madam & Eve'] > a")
+            anchors
+                .flatMap { element ->
+                    val link: String? = element.attr("href")
+                    if (!link.isNullOrBlank()) {
+                        val matchResult = regex.find(link)
+                        val (comicDate: OffsetDateTime, comicTitle: String) = if (matchResult != null) {
+                            val ld = DateTimeFormatter.ISO_LOCAL_DATE.parse(matchResult.groupValues[1])
+                            LocalDate.from(ld).atTime(0, 0).atZone(ZoneId.of("Africa/Johannesburg")).toOffsetDateTime() to matchResult.groupValues[2].formatTitle()
+                        } else {
+                            nowSupplier.get() to link.trimEnd('/').replace(".+/(.+)".toRegex(), "$1").formatTitle()
+                        }
+                        listOf(ComicLink(comicDate, "M&E - $comicTitle", link))
+                    } else {
+                        emptyList()
+                    }
+                }
+        } catch (e: Exception) {
+            logger.warn("Unable to retrieve or parse ", e)
+            emptyList()
+        }
     }
 
     private fun String.formatTitle(): String = this.replace("\\W".toRegex(), " ").trim().replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
